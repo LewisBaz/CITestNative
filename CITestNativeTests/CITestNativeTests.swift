1997e1768d6a6557905cc518bf87395d66d3cb1f
Lewis <lev.bazhkov@aisa.ru>
2025-04-15 10:24:20 +0300

    some folder changes

---
+++ CITestNativeTests/CITestNativeTests.swift
@@ -0,0 +1,35 @@
+//
+//  CITestNativeTests.swift
+//  CITestNativeTests
+//
+//  Created by Lewis on 14.04.2025.
+//
+
+import Testing
+
+struct CITestNativeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+    
+    @Test func ab() {
+        let a = 1
+        let b = 2
+        assert(a + b == 3)
+    }
+    
+    @Test func abc() {
+        let a = 1
+        let b = 2
+        let c = 5
+        let sum = a + b
+        assert(sum + b == c)
+    }
+    
+    @Test func aaa() {
+        let a = 1
+        assert(a * 3 == 3)
+    }
+
+}
